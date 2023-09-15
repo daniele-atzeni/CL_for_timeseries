528d68d83784bf0ddd84e7154ef3d9ef4bb59193
Daniele Atzeni <daniele.atze@gmail.com>
2023-09-15 16:12:31 +0200

    modified gas to avoid division by zero, added test on monash

--- gas.py
+++ gas.py
@@ -3,50 +3,50 @@ from torch import Tensor
 import torch.nn as nn
 
 
-
 class GASCell(nn.Module):
-    
-    def __init__(self, 
-                 eta_mu: float,
-                 eta_var: float
-                 ) -> None:
-        
+    def __init__(
+        self,
+        eta_mu: float,
+        eta_var: float,
+        eps: float = 1e-13,
+    ) -> None:
         super(GASCell, self).__init__()
         self.eta_mu = eta_mu
         self.eta_var = eta_var
-    
+        self.eps = eps  # this is to avoid division by zero
 
-    def update_mu_var(self, x:Tensor, mu:Tensor, var:Tensor) -> tuple[Tensor, Tensor]:
+    def update_mu_var(
+        self, x: Tensor, mu: Tensor, var: Tensor
+    ) -> tuple[Tensor, Tensor]:
         # update mu and var
-        mu += self.eta_mu * (x - mu)
-        var = var * (1 - self.eta_var) + self.eta_var * (x - mu)**2
+        mu = mu + self.eta_mu * (x - mu)
+        var = var * (1 - self.eta_var) + self.eta_var * (x - mu) ** 2
         return mu, var
 
-
-    def forward(self, x:Tensor, mu:Tensor, var:Tensor) -> tuple[Tensor, Tensor, Tensor]:
+    def forward(
+        self, x: Tensor, mu: Tensor, var: Tensor
+    ) -> tuple[Tensor, Tensor, Tensor]:
         # check input: expected tensor of shape (n_features)
         if x.dim() != 1:
             raise ValueError(f"Wrong number of dimensions. Expected 1, got {x.dim()}.")
         # update
         mu, var = self.update_mu_var(x, mu, var)
         # normalize input
-        norm_x = (x - mu) / torch.sqrt(var)
+        norm_x = (x - mu) / (torch.sqrt(var) + self.eps)
 
         return norm_x, mu, var
 
 
-
 class GASLayer(nn.Module):
-    
-    def __init__(self, 
-                 eta_mu: float, 
-                 eta_var: float
-                 ) -> None:
-        
-        super(GASLayer, self).__init__()    
+    def __init__(
+        self,
+        eta_mu: float,
+        eta_var: float,
+    ) -> None:
+        super(GASLayer, self).__init__()
         self.gas_cell = GASCell(eta_mu, eta_var)
-    
-    def init_mu_var(self, x:Tensor) -> tuple[Tensor, Tensor]:
+
+    def init_mu_var(self, x: Tensor) -> tuple[Tensor, Tensor]:
         # initialize them as mean and variance of the whole time series
         # x is assumed (ts_length, n_features)
         # # we can try other methods too
@@ -54,88 +54,90 @@ class GASLayer(nn.Module):
         var = torch.var(x, dim=0)
         return mu, var
 
-    def forward(self, 
-                x:Tensor,
-                mu:float | None = None,
-                var:float | None = None) -> tuple[Tensor, Tensor]:
-        
+    def forward(
+        self, x: Tensor, mu: float | None = None, var: float | None = None
+    ) -> tuple[Tensor, Tensor]:
         # check input
         input_dim = x.dim()
-        if not (input_dim == 2 or input_dim== 3):
-            raise ValueError(f"Wrong number of dimensions. Expected 2 or 3, got {input_dim}.")
+        if not (input_dim == 2 or input_dim == 3):
+            raise ValueError(
+                f"Wrong number of dimensions. Expected 2 or 3, got {input_dim}."
+            )
         # assumed tensor of shape (ts_length, n_features) or (1, ts_length, n_features)
         if input_dim == 3:
             if x.shape[0] != 1:
                 raise ValueError("This method supports only online learning.")
             x = x.squeeze(0)
-        
+
         # initialize mu and vars of the gas cell if they are None
         if mu is None or var is None:
-            assert mu is None and var is None, "You must pass either mu and var or none of them"
+            assert (
+                mu is None and var is None
+            ), "You must pass either mu and var or none of them"
             mu, var = self.init_mu_var(x)
 
         # initialize results
         norm_x = torch.empty_like(x)
         mus = torch.empty_like(x)
         vars = torch.empty_like(x)
-        
+
         for i in range(x.shape[0]):
             x_norm, mu, var = self.gas_cell(x[i], mu, var)
             # save results
             norm_x[i], mus[i], vars[i] = x_norm, mu, var
-        
+
         # return tensor of the same shape as original input
         if input_dim == 3:
             norm_x = norm_x.unsqueeze(0)
             mus = mus.unsqueeze(0)
             vars = vars.unsqueeze(0)
-        
+
         # combine additional information into one single tensor of shape (1, ts_length, 2*n_features)
         additional_info = torch.cat((mus, vars), dim=-1)
 
         return norm_x, additional_info
 
 
-
 class GASModel(nn.Module):
-
-    def __init__(self, 
-                 ts_encoder: nn.Module, # model to embed time series data
-                 eta_mu: float, 
-                 eta_var: float,
-                 output_model: nn.Module, # downstream model
-                 ) -> None:
-        
+    def __init__(
+        self,
+        ts_encoder: nn.Module,  # model to embed time series data
+        eta_mu: float,
+        eta_var: float,
+        output_model: nn.Module,  # downstream model
+    ) -> None:
         super(GASModel, self).__init__()
 
-        self.ts_encoder = ts_encoder
         self.gas = GASLayer(eta_mu, eta_var)
+        self.ts_encoder = ts_encoder
         self.output_model = output_model
-    
 
-    def forward(self, x:Tensor) -> Tensor:
-
+    def forward(self, x: Tensor) -> Tensor:
         # assuming shape (batch, ts_length, n_features)
         if x.dim() != 3:
             raise ValueError(f"Wrong number of dimensions. Expected 3, got {x.dim()}.")
-        
+
         # only online mode supported 'til now
         if x.shape[0] != 1:
             raise ValueError("This method supports only online learning.")
-        
-        x, add_info = self.gas(x)    # tensors shape (1, ts_length, n_features) and (1, ts_length, 2*n_features)
+
+        x, add_info = self.gas(
+            x
+        )  # tensors shape (1, ts_length, n_features) and (1, ts_length, 2*n_features)
 
         ####################### possibly a lot of new features!!!
-        add_info = add_info.reshape(x.shape[0], -1)   # this becomes (batch, ts_length * 2* n_features)
+        add_info = add_info.reshape(
+            x.shape[0], -1
+        )  # this becomes (batch, ts_length * 2 * n_features)
         #######################
 
         # process the normalized timeseries
-        x = self.ts_encoder(x)      # shape (batch, n_encoded_features)
+        x = self.ts_encoder(x)  # shape (batch, n_encoded_features)
         # check that the output_model has the correct input shape
-        #output_in_dim = self.output_model.input_shape
-        #log_sentence = f'Input n_feat of the output model must be encoded_dim + (ts_length * n_features), got {output_in_dim}.'
-        #assert output_in_dim == (x.shape[0], x.shape[1] + mus.shape[1]), log_sentence
-        
+        # output_in_dim = self.output_model.input_shape
+        # log_sentence = f'Input n_feat of the output model must be encoded_dim + (ts_length * n_features), got {output_in_dim}.'
+        # assert output_in_dim == (x.shape[0], x.shape[1] + mus.shape[1]), log_sentence
+
         # concatenate normalized x and the means
         x = torch.cat((x, add_info), dim=1)
 
--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ import math
 
 
 def generate_timeseries(growing):
-    '''
+    """
     Generate data coming from a dumped sinusoid and return lists
-    '''
+    """
 
     # Define constants
     if growing:
@@ -18,38 +18,69 @@ def generate_timeseries(growing):
 
     f = 1
     phi = 0
-    w = 2*math.pi*f
-    T = 1/f
-    dt = T/100
-    t = np.arange(0, 25*T, dt)
-    y = A*np.exp(b*t)*np.sin(w*t + phi)+50
+    w = 2 * math.pi * f
+    T = 1 / f
+    dt = T / 100
+    t = np.arange(0, 25 * T, dt)
+    y = A * np.exp(b * t) * np.sin(w * t + phi) + 50
 
-    #Add an increasing gaussian noise to the data
+    # Add an increasing gaussian noise to the data
     noise = np.random.normal(0, 2, len(y))
-    y = y+noise
+    y = y + noise
 
     return t, y
 
 
 def generate_dataset(y, ts_length, el_to_predict):
-    '''
+    """
     Takes a "signal" as a list and returns two torch Tensor containing
-    - the inputs with shape (?, ts_length) 
+    - the inputs with shape (?, ts_length)
     - the labels with shape (?, el_to_predict)
     Question marks are there because it depends on len(y) and ts_length
-    '''
-    
+    """
+
     ys = []
     preds = []
     for i in range(0, len(y) - ts_length, el_to_predict):
-        ys.append(y[i: i + ts_length])
-        preds.append(y[i + ts_length: i + ts_length + el_to_predict])
-    
+        ys.append(y[i : i + ts_length])
+        preds.append(y[i + ts_length : i + ts_length + el_to_predict])
+
     # converting from a list of np.arrays is slow
     ys = np.array(ys)
     preds = np.array(preds)
     #
-    ys = Tensor(ys) # shape (?, ts_length)
-    preds = Tensor(preds) # shape (?, el_to_predict)
+    ys = Tensor(ys)  # shape (?, ts_length)
+    preds = Tensor(preds)  # shape (?, el_to_predict)
     # we must unsqueeze the last dimension since n_features = 1
     return ys.unsqueeze(-1), preds
+
+
+def generate_dataset_multivariate(ts, ts_length, el_to_predict):
+    """
+    Takes a single (multivariate) time series as a tensor (n_features, tot_length)
+    and returns two torch Tensor containing
+    - the inputs with shape (?, n_features, ts_length)
+    - the labels with shape (?, n_features, el_to_predict)
+    Question marks are there because it depends on len(y) and ts_length
+    """
+    _, tot_length = ts.shape
+
+    xs = []
+    ys = []
+
+    # we split the ts into pieces of length ts_length + el_to_predict
+    combined_ts_length = ts_length + el_to_predict
+
+    for i in range(
+        combined_ts_length, tot_length, combined_ts_length
+    ):  # we won't consider the final piece of the ts if numbers don't match
+        xs.append(
+            ts[:, i - combined_ts_length : i - el_to_predict]
+        )  # we go "backward" in order to avoid the last tricky piece of the ts
+        ys.append(ts[:, i - el_to_predict : i])
+
+    # converting list[np.array] -> Tensor is slow, hence np.array
+    xs = Tensor(np.array(xs))
+    ys = Tensor(np.array(ys))
+
+    return xs, ys
